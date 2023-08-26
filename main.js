3567555ee39fff4c798ff22fc1e80ec001acdff3
Nikesh <bhandarinikesh93@gmail.com>
2023-08-26 10:45:57 +0545

    1st step for website

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new typed(".text",{
+    strings:["Frontend Developer", "UI/UX Desginer", "Social Media Handler"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
